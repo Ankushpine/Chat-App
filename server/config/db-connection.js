0aea4a2d3bd03a965d0eca7a4b83a38898d46ad3
Ankush Pine <ankushpineofficial@gmail.com>
2024-02-13 14:43:51 +0530

    Updated

--- server/config/db-connection.js
+++ server/config/db-connection.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 
 module.exports = mongoose.connect(process.env.MONGO_DB_URL)
 .then(() => console.log('Connected to database!'))
-.catch(()=>{console.log("Error in connecting DB");})
+.catch((error)=>{console.log("Error in connecting DB" ,error);})
 
