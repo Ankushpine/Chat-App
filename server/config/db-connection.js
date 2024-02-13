24871546a726aaddfff285990a0e20eb624e32dc
Ankush Pine <ankushpineofficial@gmail.com>
2024-02-13 16:52:03 +0530

    Updated

--- server/config/db-connection.js
+++ server/config/db-connection.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-module.exports = mongoose.connect(process.env.MONGO_DB_URL)
-.then(() => console.log('Connected to database!'))
-.catch((error)=>{console.log("Error in connecting DB" ,error.message);})
+// module.exports = mongoose.connect(process.env.MONGO_DB_URL)
+// .then(() => console.log('Connected to database!'))
+// .catch((error)=>{console.log("Error in connecting DB" ,error.message);})
 
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URL);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
+};
+
+module.exports = connectToMongoDB;
